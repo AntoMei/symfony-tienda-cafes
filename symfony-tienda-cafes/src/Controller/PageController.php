dc19db599328b5720c881e11c3c3ef486c4095a3
AntoMei <antonio.meirosu444@gmail.com>
2023-01-13 18:47:29 +0100

    Realizado el punto 2.2

--- symfony-tienda-cafes/src/Controller/PageController.php
+++ symfony-tienda-cafes/src/Controller/PageController.php
@@ -2,16 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Marca;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
 class PageController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index(): Response
+
+    public function marcaTemplate(ManagerRegistry $doctrine): Response
     {
-        return $this->render('page/index.html.twig', []);
+        $repository = $doctrine->getRepository(Marca::class);
+        $marca = $repository->findAll();
+        return $this->render('partials/_marca.html.twig', 
+            compact('marca')
+        );
+    }
+
+    #[Route('/', name: 'index')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        return $this->render('page/index.html.twig');
     }
 
     #[Route('/about', name: 'about')]
@@ -24,6 +36,12 @@ class PageController extends AbstractController
     public function menu(): Response
     {
         return $this->render('page/menu.html.twig', []);
+    }
+
+    #[Route('/marca', name: 'marca')]
+    public function marca(): Response
+    {
+        return $this->render('page/marca.html.twig', []);
     }
 
     #[Route('/review', name: 'review')]
