28417676fdc9032ccdda411cec7b9cb62be64bf4
AntoMei <antonio.meirosu444@gmail.com>
2023-01-13 17:51:33 +0100

    Realizado el punto 1.3

--- symfony-tienda-cafes/src/Controller/PageController.php
+++ symfony-tienda-cafes/src/Controller/PageController.php
@@ -13,4 +13,28 @@ class PageController extends AbstractController
     {
         return $this->render('page/index.html.twig', []);
     }
+
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('page/about.html.twig', []);
+    }
+
+    #[Route('/menu', name: 'menu')]
+    public function menu(): Response
+    {
+        return $this->render('page/menu.html.twig', []);
+    }
+
+    #[Route('/review', name: 'review')]
+    public function review(): Response
+    {
+        return $this->render('page/review.html.twig', []);
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('page/contact.html.twig', []);
+    }
 }
