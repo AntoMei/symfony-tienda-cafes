c2824e088dd69dd94450a6046e26868f491edfa4
AntoMei <antonio.meirosu444@gmail.com>
2023-01-16 13:03:40 +0100

    Realizado el punto 3.1.1

--- symfony-tienda-cafes/src/Controller/Admin/MarcaCrudController.php
+++ symfony-tienda-cafes/src/Controller/Admin/MarcaCrudController.php
@@ -6,33 +6,24 @@ use App\Entity\Marca;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
-use Symfony\Component\Validator\Constraints\Image;
+
 
 class MarcaCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
-        return Marca::class;
+        return Team::class;
     }
 
     public function configureFields(string $pageName): iterable
     {
         return [
             Field::new('name'),
-            ImageField::new('photo')->setUploadDir('/public/images')->setBasePath('/images/'),
+            ImageField::new('photo')
+            ->setUploadDir('/public/images')
+            ->setBasePath('/images/'),
             Field::new('designation')
         ];
     }
+    
 }
-
-
-    /*
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
-    }
-    */
